2bf39e5947647951610590714d7528146bf69487
riven <rivenzhou@rektec.com.cn>
2022-08-09 17:43:13 +0800

    提交代码

--- JavaCode/Spring5/src/com/edu/IOC05/test.java
+++ JavaCode/Spring5/src/com/edu/IOC05/test.java
@@ -3,11 +3,15 @@ package com.edu.IOC05;
 import com.config.SpringConfig;
 import com.edu.IOC05.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test {
+    private static final Logger log = LoggerFactory.getLogger(test.class);
+
     @Test
     public void test1() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
@@ -28,6 +32,11 @@ public class test {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.add();
+    }
+
+    @Test
+    public void test4(){
+        log.info("日志嗷嗷嗷");
     }
 
 }
--- JavaCode/Spring5/src/com/test.java
+++ JavaCode/Spring5/src/com/test.java
@@ -2,15 +2,21 @@ package com;
 
 import com.edu.bean.Emp;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:bean.xml")
+//@SpringJUnitConfig(locations = "classpath:bean.xml")
 public class test {
+
     @Test
     public void test1() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean5.xml");
         Emp emp = context.getBean("emp", Emp.class);
         System.out.println(emp);
     }
-
 }
