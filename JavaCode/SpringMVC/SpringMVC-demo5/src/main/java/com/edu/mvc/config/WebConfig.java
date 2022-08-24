9f0b4bd4451035528aa5bcdb018aacda414137a0
riven <rivenzhou@rektec.com.cn>
2022-08-24 18:09:01 +0800

    SpringMVC WebConfig配置文件上传解析器、异常处理器 92

---
+++ JavaCode/SpringMVC/SpringMVC-demo5/src/main/java/com/edu/mvc/config/WebConfig.java
@@ -0,0 +1,7 @@
+package com.edu.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+}
---
+++ JavaCode/SpringMVC/SpringMVC-demo5/src/main/java/com/edu/mvc/config/WebInit.java
@@ -0,0 +1,27 @@
+package com.edu.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return super.getServletFilters();
+    }
+}
