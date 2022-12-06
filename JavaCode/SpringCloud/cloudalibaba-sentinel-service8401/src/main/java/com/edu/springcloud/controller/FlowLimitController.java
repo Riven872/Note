95fb5fd759c04f9a5e1317974492e49eff57587c
riven <riven872@vip.qq.com>
2022-12-07 00:46:36 +0800

    SpringCloud Sentinel热点key 125

--- JavaCode/SpringCloud/cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
+++ JavaCode/SpringCloud/cloudalibaba-sentinel-service8401/src/main/java/com/edu/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.edu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +15,18 @@ public class FlowLimitController {
     }
 
     @GetMapping("/testB")
-    public String testB()
-    {
+    public String testB() {
         return "------testB";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "hotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "deal_testHotKey";
     }
 }
