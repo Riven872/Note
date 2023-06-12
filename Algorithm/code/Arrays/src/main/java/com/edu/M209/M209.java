dbb46e88b4703288c1ec997a0950d5524fb88cb3
riven <riven872@vip.qq.com>
2023-06-13 03:00:48 +0800

    算法训练 数组部分 新增 209、904 题目

---
+++ Algorithm/code/Arrays/src/main/java/com/edu/M209/M209.java
@@ -0,0 +1,29 @@
+package com.edu.M209;
+
+/**
+ * 209. 长度最小的子数组
+ * 2023年6月12日17:16:47
+ */
+public class M209 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int target = 7;
+        System.out.println(minSubArrayLen(target, nums));
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int minLength = Integer.MAX_VALUE;
+        int currentLength;// 滑动窗口的长度
+        int slowIndex = 0;// 慢指针，也是滑动窗口的起始位置
+        int sum = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+            sum += nums[fastIndex];
+            while (sum >= target) {
+                currentLength = fastIndex - slowIndex + 1;
+                minLength = Math.min(currentLength, minLength);
+                sum -= nums[slowIndex++];
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
---
+++ Algorithm/code/Arrays/src/main/java/com/edu/M904/M904.java
@@ -0,0 +1,31 @@
+package com.edu.M904;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 904. 水果成篮
+ * 2023年6月12日19:27:31
+ */
+public class M904 {
+    public static void main(String[] args) {
+        System.out.println(totalFruit(new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4}));
+    }
+
+    public static int totalFruit(int[] fruits) {
+        int slowIndex = 0;
+        int maxLength = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int fastIndex = 0; fastIndex < fruits.length; fastIndex++) {
+            map.put(fruits[fastIndex], map.getOrDefault(fruits[fastIndex], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[slowIndex], map.getOrDefault(fruits[slowIndex], 0) - 1);
+                if (map.get(fruits[slowIndex]) == 0)
+                    map.remove(fruits[slowIndex]);
+                slowIndex++;
+            }
+            maxLength = Math.max(fastIndex - slowIndex + 1, maxLength);
+        }
+        return maxLength;
+    }
+}
