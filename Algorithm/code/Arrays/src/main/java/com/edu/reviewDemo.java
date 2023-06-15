8d6eba3a322bb0baee65f2c2aa075baacc3f4503
riven <riven872@vip.qq.com>
2023-06-15 18:21:07 +0800

    算法训练 数组部分 二刷结束

--- Algorithm/code/Arrays/src/main/java/com/edu/reviewDemo.java
+++ Algorithm/code/Arrays/src/main/java/com/edu/reviewDemo.java
@@ -2,6 +2,7 @@ package com.edu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -11,23 +12,46 @@ import java.util.List;
  */
 public class reviewDemo {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sortedSquares(new int[]{-7,-3,2,3,11})));
+        System.out.println(spiralOrder(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}));
     }
 
-    public static int[] sortedSquares(int[] nums) {
-        int length = nums.length;
-        int[] arr = new int[length];
-        int slowIndex = 0;
-        int fastIndex = nums.length - 1;
-        while (slowIndex <= fastIndex) {
-            if (Math.abs(nums[slowIndex]) > Math.abs(nums[fastIndex])) {
-                arr[--length] = nums[slowIndex] * nums[slowIndex];
-                slowIndex++;
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        int i, j;
+        int start = 0;
+        int offset = 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int loop = Math.min(m, n) / 2;
+        int mid = loop;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (loop > 0) {
+            for (j = start; j < n - offset; j++) {
+                list.add(matrix[start][j]);
+            }
+            for (i = start; i < m - offset; i++) {
+                list.add(matrix[i][j]);
+            }
+            for (; j > offset - 1; j--) {
+                list.add(matrix[i][j]);
+            }
+            for (; i > offset - 1; i--) {
+                list.add(matrix[i][j]);
+            }
+            offset++;
+            loop--;
+            start++;
+        }
+        if (Math.min(m, n) % 2 != 0) {
+            if (m > n) {
+                for (int k = start; k < m - mid; k++) {
+                    list.add(matrix[k][start]);
+                }
             } else {
-                arr[--length] = nums[fastIndex] * nums[fastIndex];
-                fastIndex--;
+                for (int k = start; k < n - mid; k++) {
+                    list.add(matrix[start][k]);
+                }
             }
         }
-        return arr;
+        return list;
     }
 }
