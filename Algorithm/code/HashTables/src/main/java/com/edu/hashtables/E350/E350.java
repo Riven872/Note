af8b5002ae61fbec03d9073e438f2c42d3f94438
riven <riven872@vip.qq.com>
2023-07-03 17:37:01 +0800

    算法训练 哈希表部分 完成 350

--- Algorithm/code/HashTables/src/main/java/com/edu/hashtables/E350/E350.java
+++ Algorithm/code/HashTables/src/main/java/com/edu/hashtables/E350/E350.java
@@ -1,5 +1,8 @@
 package com.edu.hashtables.E350;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author riven
  * @date 2023/7/3 0003 3:54
@@ -7,6 +10,17 @@ package com.edu.hashtables.E350;
  */
 public class E350 {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                res.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        return res.stream().mapToInt(x -> x).toArray();
     }
 }
